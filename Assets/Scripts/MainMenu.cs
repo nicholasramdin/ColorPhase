82443aa4ae5c13199aec62b1c27c084f35ea153a
nicholasramdin <nicholasramdin@gmail.com>
2024-03-19 21:21:57 -0400

    Main Menu TitleScreen
    Main Menu and TitleScreen added
    
    functional Play and Quit buttons added for main menu
    
    updated font for win screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Play button clicked!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit button clicked!");
+        Application.Quit();
+    }
+}
+
